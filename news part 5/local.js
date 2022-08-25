2710e09d395cfe76d0f8420528a4c3c2cc1db08f
kunal <kdeotale46@gmail.com>
2022-08-25 19:52:09 +0530

    add file

---
+++ news part 5/local.js
@@ -0,0 +1,30 @@
+let getData = async () => {
+    try {
+      let res = await fetch(
+        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=dcfb7e46f19f4785a64dea9422ea2784"
+      );
+      let data = await res.json();
+      console.log(data);
+      appendData(data.articles);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  getData();
+  
+  function appendData(data) {
+    let container = document.getElementById('kd_mainHeading')
+    data.forEach((el) => {
+      // console.log('el:', el)
+      let div = document.createElement('div');
+      let img = document.createElement("img");
+      img.src = el.urlToImage;
+      let Title = document.createElement("h4");
+      Title.innerText = el.title;
+      div.append(img,Title);
+      container.append(div);
+  
+    });
+      
+  }
+  
