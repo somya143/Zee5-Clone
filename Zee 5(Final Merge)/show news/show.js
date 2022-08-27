4fad12af04ae2d94191eea4c2556bb32f90c879d
Sunil Hooda <Sunilhooda000@gmail.com>
2022-08-27 23:59:46 +0530

    ZEE5(Final Merge)

---
+++ Zee 5(Final Merge)/show news/show.js
@@ -0,0 +1,31 @@
+let array = JSON.parse(localStorage.getItem("news"));
+
+displayNews(array);
+
+function displayNews(array) {
+  let box = document.getElementById("kd_show");
+  array.forEach(function (el) {
+    console.log("el:", el);
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = el.urlToImage;
+    let title = document.createElement("p");
+    title.innerText = el.title;
+    let author = document.createElement("p");
+    author.innerText = `Author: ${el.author}`;
+    let description = document.createElement("p");
+    description.innerText = el.description;
+    let sources = document.createElement("p");
+    sources.innerText = `Sources: ${el.source.name}`;
+    let url = document.createElement("p");
+    url.innerHTML = `For More Information: ${el.url}`;
+
+    div.append(img, title, author, description, sources, url);
+    box.append(div);
+  });
+}
+
+function show() {
+  localStorage.clear();
+  window.location.href = "../crousel 2/news.html";
+}
