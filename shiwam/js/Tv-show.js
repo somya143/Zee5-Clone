02867382dc4da260ec45393769dc64cd4448f7b2
shivam9916 <shivamspecial.9916@gmail.com>
2022-08-27 16:03:46 +0530

    day-4

---
+++ shiwam/js/Tv-show.js
@@ -0,0 +1,102 @@
+var webseries = [{
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-3510/portrait/1920x770d314658847e9421793d7353c70108b2a.jpg",
+        moviename: "Meet",
+        cat: "Drama",
+    },
+
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-366/portrait/1920x7702ea2821b1a1d40d582e57c5037068307.jpg",
+        moviename: "Kundali Bhagya",
+        cat: "Drama",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-127/portrait/1920x77075429e28507042b2bbc7ef5449883f20.jpg",
+        moviename: "Kumkum Bhagya",
+        cat: "Drana",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-3392/portrait/1920x77021ec78f7dcdb410f920f825745418a29.jpg",
+        moviename: "Bhagya Lakshmi",
+        cat: "Drama",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z5101301/portrait/1920x7701ba71d251a3947c1821bc724d3204a5a.jpg",
+        moviename: "Mithai",
+        cat: "Horor",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z5155964/portrait/1920x770b637993bd05c4010be820c9a6edbb79d.jpg",
+        moviename: "DID Super Moms",
+        cat: "Horor",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z5130241/portrait/1920x7700fdba1142d7847ffbdb8fd58b343df4c.jpg",
+        moviename: "Pyar Ka Pahla Naam",
+        cat: "Drama",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-74622/portrait/satyamevajayate298776966.jpg",
+        moviename: "Satyameva Jayate",
+        cat: "Action",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-3497/portrait/1920x7705c2503a61641427e8e4f2a4c54609f8c.jpg",
+        moviename: "Sa Re Ga Ma Pa",
+        cat: "Comedy",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z525220/portrait/1920x770787ca3eff34e46dc8591e6af985e6722.jpg",
+        moviename: "Tere Bina Jiya Jay Na",
+        cat: "Comedy",
+    },
+
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z546884/portrait/1920x7704010358abd4c48de908901a3ebe56d4d.jpg",
+        moviename: "Iss Mod se Jate Hai",
+        cat: "Action",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z568134/portrait/1920x770aef6af59754445239b0d8c915368d9b1.jpg",
+        moviename: "Jee Rishtey Award Show",
+        cat: "Action",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z52149/portrait/1920x770d24b109e2013429f8fda99ecffe8d86e.jpg",
+        moviename: "Agar Tum Na Hote",
+        cat: "Romance",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z560749/portrait/1920x77050a0afd8b2b341bd91d2ad4c0d7231b0.jpg",
+        moviename: "Swarn Swar Bharat",
+        cat: "Romance",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z587441/portrait/1920x7707d249a2952d6465994cb840d13d71863.jpg",
+        moviename: "DID LIL Masters",
+        cat: "Comedy",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z520450/portrait/1920x7707be825d8e0b149478aaa493d90ecf34f.jpg",
+        moviename: "Kashi-Bai-Bajirao",
+        cat: "Action",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-3137/portrait/1920x770afffb51b6ec248b4b86b9a0c7d256266.jpg",
+        moviename: "Teri Meri Ek Jindadi",
+        cat: "Comedy",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z5185799/portrait/1920x7702e719dbb7f3645109a4c0b1680d85e9d.jpg",
+        moviename: "Sanjog",
+        cat: "Thriller",
+    },
+    {
+        img_url: "https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-6-4z5130241/portrait/1920x7700fdba1142d7847ffbdb8fd58b343df4c.jpg",
+        moviename: "Radha Mohan",
+        cat: "Thriller",
+    },
+
+
+]
+
+localStorage.setItem("webseries", JSON.stringify(webseries));
